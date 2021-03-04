82945bf08111cfae81856e0bb9aebe5d9f54bea2
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-03 23:39:21 -0300

    refactor (new name) + new functionalities

--- src/helpers/File.ps1
+++ src/helpers/File.ps1
@@ -26,6 +26,31 @@ function Get-FileLength {
 	return $length.ToString() + 'B ';
 }
 
+function Get-FileName {
+	param (
+		[Parameter(Mandatory = $true)] $file
+	)
+
+	$name = $file.Name;
+
+	if (
+		($Global:ColorSettings.File.Type.SymbolicLink.ShowTarget -eq $true) `
+		-and ($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+	) {
+		$target = $null;
+
+		try {
+			$target = Get-ShortenedPath (Get-Item -Path $file | Select-Object -ExpandProperty Target);
+		} catch {}
+
+		if ($null -ne $target) {
+			$name = $name + ' -> ' + $target;
+		}
+	}
+
+	return $name;
+}
+
 function Write-ColorizedFileLine {
 	param (
 		[String] $color,
@@ -37,7 +62,7 @@ function Write-ColorizedFileLine {
 		$file.Mode,
 		([String]::Format("{0,10}  {1,8}", $file.LastWriteTime.ToString("d"), $file.LastWriteTime.ToString("t"))),
 		(Get-FileLength $file),
-		$file.Name
+		(Get-FileName $file)
 	);
 }
 
@@ -46,15 +71,29 @@ function Write-FileHeader {
 		[Parameter(Mandatory = $true, Position = 1)] [String] $directory
 	)
 
-	$currentDirectory = (Get-Location).Path;
+	if ($Script:showHeader -eq $false) {
+		return;
+	}
 
-	Write-Host;
-	Write-Host "    Directory: " -NoNewline;
-	Write-Host "$currentDirectory";
+	if ($Global:ColorSettings.File.Path.Hidden -eq $false) {
+		$currentDirectory = Get-ShortenedPath $directory;
 
-	Write-Host;
-	Write-Host "Mode                LastWriteTime     Length Name";
-	Write-Host "----                -------------     ------ ----";
+		$pathTitleColor = $Global:ColorSettings.File.Path.Title.Color;
+		$pathTextColor = $Global:ColorSettings.File.Path.Text.Color;
+
+		Write-Host;
+		Write-Host "    Directory: " -ForegroundColor $pathTitleColor -NoNewline;
+		Write-Host "$currentDirectory" -ForegroundColor $pathTextColor;
+	}
+
+	if ($Global:ColorSettings.File.Header.Hidden -eq $false) {
+		$headerTextColor = $Global:ColorSettings.File.Header.Text.Color;
+		$headerSeparatorsColor = $Global:ColorSettings.File.Header.Separators.Color;
+
+		Write-Host;
+		Write-Host "Mode                LastWriteTime     Length Name" -ForegroundColor $headerTextColor;
+		Write-Host "----                -------------     ------ ----" -ForegroundColor $headerSeparatorsColor;
+	}	
 
 	$Script:showHeader = $false;
 }
@@ -66,11 +105,11 @@ function Write-File {
 
 	$regexOptions = ([System.Text.RegularExpressions.RegexOptions]::IgnoreCase);
 
-	$binary = New-Object System.Text.RegularExpressions.Regex($Global:PSColor.File.Type.Binary.RegEx, $regexOptions);
-	$code = New-Object System.Text.RegularExpressions.Regex($Global:PSColor.File.Type.Code.RegEx, $regexOptions);
-	$compressed = New-Object System.Text.RegularExpressions.Regex($Global:PSColor.File.Type.Compressed.RegEx, $regexOptions);
-	$text = New-Object System.Text.RegularExpressions.Regex($Global:PSColor.File.Type.Text.RegEx, $regexOptions);
-	$hidden = New-Object System.Text.RegularExpressions.Regex($Global:PSColor.File.Type.Hidden.RegEx, $regexOptions);
+	$binary = New-Object System.Text.RegularExpressions.Regex($Global:ColorSettings.File.Type.Binary.RegEx, $regexOptions);
+	$code = New-Object System.Text.RegularExpressions.Regex($Global:ColorSettings.File.Type.Code.RegEx, $regexOptions);
+	$compressed = New-Object System.Text.RegularExpressions.Regex($Global:ColorSettings.File.Type.Compressed.RegEx, $regexOptions);
+	$text = New-Object System.Text.RegularExpressions.Regex($Global:ColorSettings.File.Type.Text.RegEx, $regexOptions);
+	$hidden = New-Object System.Text.RegularExpressions.Regex($Global:ColorSettings.File.Type.Hidden.RegEx, $regexOptions);
 
 	if ($file -is [System.IO.DirectoryInfo]) {
 		$currentDirectory = $file.Parent.FullName;
@@ -80,19 +119,21 @@ function Write-File {
 
 	Write-FileHeader $currentDirectory;
 
-	if ($hidden.IsMatch($file.Name)) {
-		Write-ColorizedFileLine $Global:PSColor.File.Type.Hidden.Color $file;
+	if ($file.Attributes -band [IO.FileAttributes]::ReparsePoint) {
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.SymbolicLink.Color $file;
+	} elseif ($hidden.IsMatch($file.Name)) {
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.Hidden.Color $file;
 	} elseif ($file -is [System.IO.DirectoryInfo]) {
-		Write-ColorizedFileLine $Global:PSColor.File.Type.Directory.Color $file;
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.Directory.Color $file;
 	} elseif ($binary.IsMatch($file.Name)) {
-		Write-ColorizedFileLine $Global:PSColor.File.Type.Binary.Color $file;
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.Binary.Color $file;
 	} elseif ($code.IsMatch($file.Name)) {
-		Write-ColorizedFileLine $Global:PSColor.File.Type.Code.Color $file;
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.Code.Color $file;
 	} elseif ($compressed.IsMatch($file.Name)) {
-		Write-ColorizedFileLine $Global:PSColor.File.Type.Compressed.Color $file;
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.Compressed.Color $file;
 	} elseif ($text.IsMatch($file.Name)) {
-		Write-ColorizedFileLine $Global:PSColor.File.Type.Text.Color $file;
+		Write-ColorizedFileLine $Global:ColorSettings.File.Type.Text.Color $file;
 	} else {
-		Write-ColorizedFileLine $Global:PSColor.File.Default.Color $file;
+		Write-ColorizedFileLine $Global:ColorSettings.File.Default.Color $file;
 	}
 }
